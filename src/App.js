c7ba865506f77b6d2fe2c0d335a3b1cf8986f931
Bas van der Hoek <bas@hoek.com>
2018-03-02 13:31:02 +0100

    kanji component with styled grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Kanji from './components/Kanji'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Kanji size={180}/>
       </div>
     );
   }
