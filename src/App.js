fa2e482bc35279dbc314c43f0e6244b1883275e2
Bas van der Hoek <bas@hoek.com>
2018-03-02 20:03:23 +0100

    svg as a parameter to kanji component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import Kanji from './components/Kanji'
 import './App.css';
+import json from './kanji-svg'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Kanji grid={9} size={180}/>
+        <Kanji grid={4} size={180} svg={json[3].svg}/>
       </div>
     );
   }
