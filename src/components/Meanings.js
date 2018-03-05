87a1ca6fef110d007365f84c5a8d1c6ed5be8708
Bas van der Hoek <bas@hoek.com>
2018-03-05 14:18:18 +0100

    add literal to meanings

--- src/components/Meanings.js
+++ src/components/Meanings.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 const Meanings = ({literal, meanings}) => {
   const renderMeaning = (meaning) => {
-    return <dt>{meaning}</dt>
+    return <dd>{meaning}</dd>
   }
 
   return (
     <dl>
+      <dt>{literal}</dt>
       {meanings.map(renderMeaning)}
     </dl>
   )
