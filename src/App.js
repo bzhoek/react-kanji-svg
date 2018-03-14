86ab14b696842b9f4b678f77eeecdb5dd72b0883
Bas van der Hoek <bas@hoek.com>
2018-03-14 22:06:31 +0100

    wired randomized lookup with next and previous navigation

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import Kanji from './components/Kanji'
 import Meanings from './components/Meanings'
 import './App.css';
 import json from './kanji-svg'
+import RandomizedLookup from './RandomizedLookup'
 
 class App extends Component {
 
   constructor() {
     super()
+    this.lookup = new RandomizedLookup()
+    let today = new Date()
+    let freq = this.lookup.forDay(today)
+
     this.state = {
-      kanji: this.find('験')
+      date: today,
+      kanji: this.freq(freq) || this.find('火')
     }
   }
 
@@ -19,6 +25,13 @@ class App extends Component {
       this.setState({
         kanji: kanji
       })
+    }
+  }
+
+  freq(n) {
+    let kanji = json.filter(c => c.freq === n.toString())
+    if (kanji.length) {
+      return kanji.shift()
     }
   }
 
@@ -42,9 +55,37 @@ class App extends Component {
     return json.filter(c => c.meanings.some(m => m === text))
   }
 
+  onPrevious() {
+    this.changeDate(-1)
+  }
+
+  onNext() {
+    this.changeDate(+1)
+  }
+
+  changeDate(delta) {
+    let other = this.state.date
+    other.setDate(other.getDate() + delta)
+    let freq = this.lookup.forDay(other)
+    let kanji = this.freq(freq)
+    if(kanji) {
+      this.setState({
+        date: other,
+        kanji: kanji
+      })
+    }
+  }
+
   render() {
     return (
       <div className="App">
+        <button onClick={this.onPrevious.bind(this)}>
+          Previous
+        </button>
+        <span>{this.state.date.toLocaleDateString()}</span>
+        <button onClick={this.onNext.bind(this)}>
+          Next
+        </button>
         <input type="text" ref={input => this.textInput = input} onChange={this.onChange.bind(this)}/>
         <Kanji grid={9} size={180} svg={this.state.kanji.svg}/>
         <p>N{this.state.kanji.jlpt}, {this.state.kanji.freq}</p>
