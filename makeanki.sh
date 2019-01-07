57c018094d0aba47a3953f665ca060280e7f8011
Bas van der Hoek <bas@hoek.com>
2019-01-07 16:48:59 +0100

    anki script and documentation of conversation

---
+++ makeanki.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+UNICODE=$(sqlite3 kanji.sqlite "select unicode from Kanji where literal='$1'")
+MEANING=$(sqlite3 kanji.sqlite "select meaning from Kanji where literal='$1'")
+
+SVG='anki.kanji.svg'
+STYLED='anki.styled.svg'
+sqlite3 kanji.sqlite "select drawing from Kanji where literal='$1'" > $SVG
+xsltproc -nonet kanjistyle.xslt $SVG > $STYLED
+convert -size 1024x1024 $STYLED anki.styled.png
+
+cat > anki.media.json <<- EOM
+{
+  "action": "storeMediaFile",
+  "version": 6,
+  "params": {
+    "filename": "${UNICODE}.png",
+    "data": "$(base64 anki.styled.png)"
+  }
+}
+EOM
+
+curl localhost:8765 -X POST --data @anki.media.json
+
+cat > anki.strokes.json <<- EOM
+{
+  "action": "updateNoteFields",
+  "version": 6,
+  "params": {
+    "note": {
+      "id": ${NOTEID},
+      "fields": {
+        "strokes": "<img src=\"${UNICODE}.png\" />"
+      }
+    }
+  }
+}
+EOM
+
+cat > anki.add.json <<- EOM
+{
+  "action": "addNote",
+  "version": 6,
+  "params": {
+    "note": {
+      "deckName": "Japans::Kanji",
+      "modelName": "OnKanji",
+      "fields": {
+        "nederlands": "${MEANING}",
+        "kanji": "${1}",
+        "on": "",
+        "notes": "",
+        "strokes": "<img src=\"${UNICODE}.png\" />"
+      },
+      "options": {
+        "allowDuplicate": false
+      },
+      "tags": []
+    }
+  }
+}
+EOM
+
+curl localhost:8765 -X POST --data @anki.add.json
+# {"result": 1545055762607, "error": null}%
